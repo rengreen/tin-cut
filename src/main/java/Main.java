09a5af90c43acda0f7dc35746da761972d68881a
rengreen <r.m.gardzinska@gmail.com>
2019-10-12 11:40:01 +0200

    Move content of main method to controller class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,74 +1,12 @@
-import model.Cut;
-import model.Fragment;
-import model.Tinware;
-import service.CuttingService;
+import controller.ConsoleController;
 
 import java.util.*;
 
 public class Main {
 
-    static Scanner scan = new Scanner(System.in);
-
     public static void main(String[] args) {
-
-        System.out.println("Podaj wymiar poziomy blachy: ");
-        int m = scan.nextInt();
-        System.out.println("Podaj wymiar pionowy blachy: ");
-        int n = scan.nextInt();
-
-        List<Cut> cutsList = new ArrayList<Cut>();
-        System.out.println("Podaj kolejne wartości cięcia blachy pionowo. Program przyjmie tylko " + (m-1) + " wartości.");
-
-        for(int i=1; i<m; i++){
-            int x = scan.nextInt();
-            Cut cut = new Cut("vertical", i, x);
-            cutsList.add(cut);
-        }
-
-        System.out.println("Podaj kolejne wartości cięcia blachy poziomo. Program przyjmie tylko " + (n-1) + " wartości.");
-
-        for(int i=1; i<n; i++){
-            int y = scan.nextInt();
-            Cut cut = new Cut("horizontal", i, y);
-            cutsList.add(cut);
-        }
-
-        System.out.println(cutsList.size());
-
-        Fragment fragment = new Fragment(1, 1, m, n);
-        List<Fragment> fragments = new ArrayList<>();
-        fragments.add(fragment);
-
-        Tinware tinware = new Tinware(fragments, 0);
-        /*
-        List<model.Cut> cuts = Arrays.asList(
-                new model.Cut("vertical", 1, 2),
-                new model.Cut("vertical", 2, 1),
-                new model.Cut("vertical", 3, 3),
-                new model.Cut("vertical", 4, 1),
-                new model.Cut("vertical", 5, 4),
-                new model.Cut("horizontal", 1, 4),
-                new model.Cut("horizontal", 2, 1),
-                new model.Cut("horizontal", 3, 2)
-        );*/
-
-        //sortowanie listy cięć według kosztu malejąco - cięcia pionowe i poziome razem
-        cutsList.sort(Comparator.comparing(Cut::getCost).reversed());
-
-        CuttingService cuttingService = new CuttingService();
-        //wykonywanie cięć pionowych lub poziomych wg powyższej kolejności
-        for (Cut cut : cutsList) {
-            System.out.println(cut.getOrientation());
-
-            if (cut.getOrientation().equals("vertical")) {
-                cuttingService.doVerticalCut(tinware, cut);
-            } else {
-                cuttingService.doHorizontalCut(tinware, cut);
-            }
-            System.out.println(tinware.getCost());
-        }
-
-        System.out.println(tinware.getCost());
+        ConsoleController consoleController= new ConsoleController();
+        consoleController.action();
 
     }
 
---
+++ src/main/java/controller/ConsoleController.java
@@ -0,0 +1,76 @@
+package controller;
+
+import model.Cut;
+import model.Fragment;
+import model.Tinware;
+import service.CuttingService;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class ConsoleController {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void action(){
+        System.out.println("Podaj wymiar poziomy blachy: ");
+        int m = scanner.nextInt();
+        System.out.println("Podaj wymiar pionowy blachy: ");
+        int n = scanner.nextInt();
+
+        List<Cut> cutsList = new ArrayList<Cut>();
+        System.out.println("Podaj kolejne wartości cięcia blachy pionowo. Program przyjmie tylko " + (m-1) + " wartości.");
+
+        for(int i=1; i<m; i++){
+            int x = scanner.nextInt();
+            Cut cut = new Cut("vertical", i, x);
+            cutsList.add(cut);
+        }
+
+        System.out.println("Podaj kolejne wartości cięcia blachy poziomo. Program przyjmie tylko " + (n-1) + " wartości.");
+
+        for(int i=1; i<n; i++){
+            int y = scanner.nextInt();
+            Cut cut = new Cut("horizontal", i, y);
+            cutsList.add(cut);
+        }
+
+        System.out.println(cutsList.size());
+
+        Fragment fragment = new Fragment(1, 1, m, n);
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(fragment);
+
+        Tinware tinware = new Tinware(fragments, 0);
+        /*
+        List<model.Cut> cuts = Arrays.asList(
+                new model.Cut("vertical", 1, 2),
+                new model.Cut("vertical", 2, 1),
+                new model.Cut("vertical", 3, 3),
+                new model.Cut("vertical", 4, 1),
+                new model.Cut("vertical", 5, 4),
+                new model.Cut("horizontal", 1, 4),
+                new model.Cut("horizontal", 2, 1),
+                new model.Cut("horizontal", 3, 2)
+        );*/
+
+        //sortowanie listy cięć według kosztu malejąco - cięcia pionowe i poziome razem
+        cutsList.sort(Comparator.comparing(Cut::getCost).reversed());
+
+        CuttingService cuttingService = new CuttingService();
+        //wykonywanie cięć pionowych lub poziomych wg powyższej kolejności
+        for (Cut cut : cutsList) {
+            System.out.println(cut.getOrientation());
+
+            if (cut.getOrientation().equals("vertical")) {
+                cuttingService.doVerticalCut(tinware, cut);
+            } else {
+                cuttingService.doHorizontalCut(tinware, cut);
+            }
+            System.out.println(tinware.getCost());
+        }
+
+        System.out.println(tinware.getCost());
+    }
+}
